a70d03612fd0deb2355d7518f32a8d47d0d251af
Stoian02 <81009844+Stoian02@users.noreply.github.com>
2024-02-27 17:39:28 +0200

    Adde email to user

--- PS_45_Stoyan/Welcome/Model/User.cs
+++ PS_45_Stoyan/Welcome/Model/User.cs
@@ -13,13 +13,15 @@ namespace Welcome.Model
         private string _fakNum { get; set; }
         private string _name { set; get; }
         private string _password { set; get; }
+        private string _email { get; set; }
         private UserRolesEnum _role { set; get; }
 
-        public User(string fakNum, string name, string pass, UserRolesEnum role)
+        public User(string fakNum, string name, string pass, string email,  UserRolesEnum role)
         {
             _fakNum = fakNum;
             _name = name;
             _password = pass;
+            _email = email;
             _role = role;
         }
     }
