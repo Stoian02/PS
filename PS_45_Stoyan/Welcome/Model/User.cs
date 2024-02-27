9f5d4412a0a9998af8758f678c34d56dc3ca72ec
Stoian02 <81009844+Stoian02@users.noreply.github.com>
2024-02-27 16:02:24 +0200

    Base folder and file structure

---
+++ PS_45_Stoyan/Welcome/Model/User.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Welcome.Others;
+
+namespace Welcome.Model
+{
+    class User
+    {
+        public string Name { set; get; }
+        public string Password { set; get; }
+        public UserRolesEnum Role { set; get; }
+
+        public User(string name, string pass, UserRolesEnum role)
+        {
+            Name = name;
+            Password = pass;
+            Role = role;
+        }
+    }
+}
---
+++ PS_45_Stoyan/Welcome/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+using Welcome.Model;
+using Welcome.Others;
+using Welcome.View;
+using Welcome.ViewModel;
+
+
+
+namespace Welcome
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.WriteLine();
+
+            User user = new User("Stoyo", "parola", UserRolesEnum.STUDENT);
+
+            UserViewModel userViewModel = new UserViewModel(user);
+
+            UserView userView = new UserView(userViewModel);
+
+            userView.Display();
+
+
+
+        }
+    }
+}
